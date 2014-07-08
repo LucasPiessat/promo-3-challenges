86edb30377ed842c075bea6205dd279ff784897f
LucasPiessat <l.piessat@gmail.com>
2014-07-08 13:11:34 +0200

    exercise 3 solved

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/interface.rb
@@ -16,8 +16,11 @@ birth_day = gets.chomp.to_i
 
 puts 'Computing your age (with the most complicated algorithms)........'
 
+require 'date'
+
+
 # TODO: This is probably where you'd like to use your brand new function!
-calculated_age = 0
+calculated_age = age_in_days(birth_day, birth_month, birth_year)
 
 
 # Finally, print user's age in days:
