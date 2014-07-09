46ca1b964a030bda4922baa6afb22cee1c3bd619
LucasPiessat <l.piessat@gmail.com>
2014-07-09 16:31:36 +0200

    pmu is cool and solved

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,17 @@
 def pmu_format!(race_array)
+  race_array.reverse!
+  race_array.map! do |x|
+    a = race_array.size - race_array.index(x)
+    "#{a}-#{x}!"
+  end
+
+
+  #race_array.each {|x| puts size - race_array.index('x') + "#{x}!" }
+
+
   #TODO: modify the given array so that it is PMU-consistent
 end
 
-
-
+result= ["Jojo_la_praline", "Tete_de_croissant", "Face_de_pain_sucé"]
+puts pmu_format!(result)
 
