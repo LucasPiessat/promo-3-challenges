849c3df400d95c7ec4533f45b4fa225c725448ad
LucasPiessat <l.piessat@gmail.com>
2014-07-09 12:22:43 +0200

    solved fucking exercise

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -6,9 +6,24 @@ begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
   name.chomp! if name
-
+  students << name if name !=""
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
+
+def final_sentence(param,tab)
+  if param==wagon_sort(tab).last
+    print "and #{param}."
+  elsif param==wagon_sort(tab).last(2).first
+    print "#{param} "
+   else
+    print "#{param}, "
+  end
+end
+
+  print "Congratulations! Your Wagon has #{students.size} students :  "
+  wagon_sort(students).each{|x| final_sentence(x,students)}
+
+
 
 # TODO: Call `wagon_sort` method and display the sorted student list
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,4 @@
 def wagon_sort(students)
+  students.sort_by {|x| [x.downcase, x]}
   # TODO: return a copy of students, sorted alphabetically
 end
