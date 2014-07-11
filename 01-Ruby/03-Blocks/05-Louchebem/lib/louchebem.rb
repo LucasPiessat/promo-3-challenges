0c2ef3f0f3319d4153ca98af766313cebd29fa27
LucasPiessat <l.piessat@gmail.com>
2014-07-11 08:59:00 +0200

    solved

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,5 +1,25 @@
-# Encoding: utf-8
+def louchebemize_word(word)
+  suffixe = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+  vowels = %w(a e i o u y A E I O U Y)
+  #special_characters = [""]
 
-def louchebemize(sentence)
-  #TODO: implement your louchebem translator
+  if word.length == 1
+    word
+  elsif vowels.include?(word[0])
+    "l" + word + "#{suffixe.sample}"
+  else
+    louchebemized_word = ""
+    prefix = ""
+    suffix = ""
+    word.chars.each_with_index do |char, index|
+      if vowels.include?(char) then
+        break
+      else
+        prefix = word[index+1..-1]
+        suffix = suffix + word[index]
+        louchebemized_word = prefix + suffix
+      end
+    end
+    return louchebemized_word = "l" + louchebemized_word + "#{suffixe.sample}"
+  end
 end
