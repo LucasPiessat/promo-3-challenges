e5977c2613fe1850aa9ffd175e874a2a99b7a619
LucasPiessat <l.piessat@gmail.com>
2014-07-16 11:59:32 +0200

    crucified your panda

---
+++ 02-OOP/01-Define-your-own-class/lib/panda.rb
@@ -0,0 +1,16 @@
+class Panda
+  attr_accessor :weight, :height
+
+  def initialize(weight, height, eye_color)
+    @panda_died = true
+    @panda_weight = weight
+    @panda_height = height
+    @panda_eye = eye_color
+  end
+  def crucified?
+    return @panda_died
+  end
+  def born_again
+    @panda_died = false
+  end
+end
