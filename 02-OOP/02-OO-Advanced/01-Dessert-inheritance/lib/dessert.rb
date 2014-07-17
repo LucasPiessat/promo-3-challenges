3502860ae392647fa7040695449d6de37df7f768
LucasPiessat <l.piessat@gmail.com>
2014-07-17 11:17:33 +0200

    Dessert 80% done

--- 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,25 @@
 class Dessert
+  attr_reader  :name, :calories
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
   end
-  
+
+  def healthy?
+    (0..200).cover?(@calories) ? true : false
+  end
+
   def delicious?
+   true
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+  attr_reader :flavor
   def initialize(name, calories, flavor)
+    @flavor = flavor
+  end
+  def delicious?
+    @flavor == "black licorice" ? false : super
   end
 end
