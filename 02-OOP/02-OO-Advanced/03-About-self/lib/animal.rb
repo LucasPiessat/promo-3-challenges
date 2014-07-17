5d8876ee0d719b2d80a825db29843f9426a30a2f
LucasPiessat <l.piessat@gmail.com>
2014-07-17 14:17:57 +0200

    solved

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -3,25 +3,31 @@ class Animal
   def name(name)
     @name = name
     #TODO: find good instruction
+    self
   end
-  
+
   def specie(specie)
     @specie = specie
+    self
     #TODO: find good instruction
   end
-  
+
   def color(color)
     @color = color
+    self
     #TODO: find good instruction
   end
-  
+
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
+    self
     #TODO: find good instruction
   end
-  
+
   def to_s
     "Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
-  
+
 end
+
+Animal.new.name("Roger").specie("Poney").color("white")
