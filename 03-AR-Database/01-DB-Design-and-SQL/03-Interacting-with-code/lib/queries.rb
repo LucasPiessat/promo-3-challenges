188c31040740838d83834d20b37b9645237d804d
LucasPiessat <l.piessat@gmail.com>
2014-07-24 12:49:53 +0200

    ok

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -5,17 +5,19 @@ database_path = File.join(File.dirname(__FILE__), 'db/jukebox.sqlite')
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
-  #TODO: count number of rows in table table_name
-end
+  db.execute("SELECT COUNT(*) FROM #{table_name}")
 
 def sorted_artists(db)
+  db.execute("SELECT name FROM artists ORDER BY ASC")
   #TODO: return array of artists' names sorted alphabetically
 end
 
 def love_tracks(db)
+  db.execute("SELECT name FROM track WHERE name LIKE %love%")
   #TODO: return array of love songs
 end
 
 def long_tracks(db, min_length)
+  de.execute("SELECT * FROM track were Millisecond > #{min_length*60000}")
   #TODO: return tracks verifying: duration > min_length (minutes)
 end
