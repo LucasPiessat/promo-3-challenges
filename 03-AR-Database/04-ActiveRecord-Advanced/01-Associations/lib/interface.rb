78b975b1a262e6f38a182f25422da151f92e1750
LucasPiessat <l.piessat@gmail.com>
2014-07-29 14:31:57 +0200

    ok

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
@@ -2,6 +2,9 @@ require_relative 'config/application'
 require_relative 'models/post'
 require_relative 'models/user'
 
+User.where(name: 'jojo', email: 'jo@jo.com').first_or_create
+p User.last
+
 def ask(prompt)
   print "#{prompt} "
   gets.to_s.chomp
@@ -13,32 +16,42 @@ def create_post(user)
   rating = ask('Rating:')
   post_params = { name: name, source_url: source_url, date: Time.now, rating: rating }
 
-  # TODO: use ActiveRecord to add a new post for the user logged in!
+  post = user.posts.create(post_params)
+  post.save
+
 end
 
 def list_posts(user)
-  # TODO: use ActiveRecord to get all posts of the current user
+  posts = user.posts.all
+  posts.each do |post|
+  puts post.name
+  end
 end
 
 def delete_posts(user)
-  # TODO: use ActiveRecord to delete all posts of current user
+  user.posts.destroy_all
 end
 
-while true
-  logged_in = false
 
-  until logged_in
+logged_in = false
+
+until logged_in
     puts 'Please login with your <id>'
+    id = ask(id)
+    user = User.find(id)
+    logged_in = true
     # TODO: instantiate a user with his <id>
-  end
+end
 
+exit = false
+while true
   puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
   puts "2. Read your posts"
   puts "3. Delete all posts"
   puts "4. Exit"
 
-  case ask('>')
+  case ask('>').to_i
   when 1 then create_post(user)
   when 2 then list_posts(user)
   when 3 then delete_posts(user)
