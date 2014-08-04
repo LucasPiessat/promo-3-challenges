4213a4c97d4c039d506f47259c751b1438d85c52
LucasPiessat <l.piessat@gmail.com>
2014-08-04 15:37:57 +0200

    ok

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,5 +1,6 @@
-/*
-  *
-  * TODO: add your code here!
-  *
-*/
+$(function() {
+  $("#message-wrapper").hide().delay(200).slideDown(1000);
+  $("#message-wrapper").on('click', function() {
+    $(this).hide()
+  });
+});
